4a48c341d56bd57ec2a46b3d5845f1410ff68604
sandeep532 <sandeepsharmadurbhaka@gmail.com>
2021-05-27 23:46:43 +0530

    The player roles the die to get the numbers between 1-6

---
+++ snakeAndLadderUc2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class snakeAndLadderUc2
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+int playerPosition = 0;
+int rollDice = (int)(Math.floor(Math.random()*10)%6)+1;
+System.out.println(rollDice);
+
+}
+}
